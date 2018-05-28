262ccb977e3f6d1607ac4c1814ff387c96811dd2
ear78 <ear78@aol.com>
2018-05-27 22:48:26 -0700

    added components scss and bootstrap overrides files

--- wp-content/themes/wppractice2/page-about.php
+++ wp-content/themes/wppractice2/page-about.php
@@ -7,21 +7,21 @@
 
 <?php get_header(); ?>
 
-    <h3>About Page Template File single use</h3>
-
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-    <h2 id="post-<?php the_ID(); ?>">
-        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-    </h2>
-    <small><?php the_time('F jS, Y'); ?> <!-- <?php the_author(); ?> --></small>
+<!--    <h2 id="post---><?php //the_ID(); ?><!--">-->
+<!--        <a href="--><?php //the_permalink(); ?><!--">--><?php //the_title(); ?><!--</a>-->
+<!--    </h2>-->
+<!--    <small>--><?php //the_time('F jS, Y'); ?><!-- -->
+        <!-- <?php the_author(); ?> -->
+<!--    </small>-->
 
     <div class="entry">
 
         <?php the_post_thumbnail(); ?>
 
-        <div class="content-wrapper">
-            <?php the_content('Read the rest of this entry &raquo;'); ?>
+        <div class="">
+            <?php the_content(); ?>
         </div>
 
     </div>
